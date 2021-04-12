2bd713ada2b31520c85a3c4ba05e871b566b9f82
bastienxs <bastienxs@gmail.com>
2021-04-12 15:00:04 +0200

    find post

--- graphql/resolvers/posts.js
+++ graphql/resolvers/posts.js
@@ -9,5 +9,19 @@ module.exports = {
         throw new Error(error);
       }
     },
+
+    async getPost(_, { postId }) {
+      // console.log(postId);
+      try {
+        const post = await Post.findById(postId);
+        if (post) {
+          return post;
+        } else {
+          throw new Error("Post not found");
+        }
+      } catch (error) {
+        throw new Error(error);
+      }
+    },
   },
 };
