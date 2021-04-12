89859f880a990e8eaff89d890004e2e47a772b34
bastienxs <bastienxs@gmail.com>
2021-04-12 14:36:00 +0200

    login func

--- graphql/resolvers/users.js
+++ graphql/resolvers/users.js
@@ -2,10 +2,21 @@ const bycrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const { UserInputError } = require("apollo-server");
 
-const { validateRegisterInput } = require("../../utils/validators");
-
+const { validateRegisterInput, validateLoginInput } = require("../../utils/validators");
 const User = require("../../models/User");
 const { SECRET_KEY } = require("../../config");
+
+const generateToken= (user) =>{
+  return jwt.sign(
+    {
+      id: user.id,
+      email: user.email,
+      username: user.username,
+    },
+    SECRET_KEY,
+    { expiresIn: "1h" }
+  );
+}
 
 module.exports = {
   Mutation: {
@@ -43,15 +54,7 @@ module.exports = {
 
       const res = await newUser.save();
 
-      const token = jwt.sign(
-        {
-          id: res.id,
-          email: res.email,
-          username: res.username,
-        },
-        SECRET_KEY,
-        { expiresIn: "1h" }
-      );
+      const token = generateToken(res)
 
       return {
         ...res._doc,
@@ -59,5 +62,34 @@ module.exports = {
         token,
       };
     },
+
+    async login(_, {username, password}) {
+      const {errors, valid} = validateLoginInput(username, password)
+
+      if(!valid) {
+        throw new UserInputError('Login form not valid', {errors})
+      }
+
+      const user = await User.findOne({username})
+
+      if(!user){
+        errors.general = 'User not found'
+        throw new UserInputError('User not found', {errors})
+      }
+
+      const match = await bycrypt.compare(password, user.password)
+      if (!match) {
+        errors.general = 'Wrong credentials'
+        throw new UserInputError('Wrong credentials', {errors})
+      }
+
+      const token = generateToken(user)
+
+      return {
+        ...user._doc,
+        id: user.id,
+        token,
+      };
+    }
   },
 };
