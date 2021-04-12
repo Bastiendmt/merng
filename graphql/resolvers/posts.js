00762ebdda84a539c418daf70df82b7ec6d4b519
bastienxs <bastienxs@gmail.com>
2021-04-12 17:36:02 +0200

    posts subscription

--- graphql/resolvers/posts.js
+++ graphql/resolvers/posts.js
@@ -29,7 +29,6 @@ module.exports = {
   Mutation: {
     async createPost(_, { body }, context) {
       const user = checkAuth(context);
-      console.log(user);
 
       const newPost = new Post({
         body,
@@ -39,6 +38,10 @@ module.exports = {
       });
 
       const post = await newPost.save();
+
+      context.pubsub.publish('NEW_POST', {
+        newPost: post
+      })
 
       return post;
     },
@@ -80,4 +83,9 @@ module.exports = {
       } else throw new UserInputError("Post not found");
     },
   },
+  Subscription : {
+    newPost: {
+      subscribe:(_, __, {pubsub}) => pubsub.asyncIterator('NEW_POST')
+    }
+  }
 };
