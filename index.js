47f6c022464be69533a38835e7d68114312669d9
bastienxs <bastienxs@gmail.com>
2021-04-12 15:15:42 +0200

    create post

--- index.js
+++ index.js
@@ -3,12 +3,13 @@ const gql = require("graphql-tag");
 const mongoose = require("mongoose");
 
 const typeDefs = require("./graphql/typeDefs");
-const resolvers  = require("./graphql/resolvers")
+const resolvers = require("./graphql/resolvers");
 const { MONGODB } = require("./config");
 
 const server = new ApolloServer({
   typeDefs,
   resolvers,
+  context: ({ req }) => ({ req }),
 });
 
 mongoose
