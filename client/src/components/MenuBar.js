122b0d26daba2ce3a6d4f8686a2babc1d40ecf7a
bastienxs <bastienxs@gmail.com>
2021-04-13 18:05:00 +0200

    margin adjuted

--- client/src/components/MenuBar.js
+++ client/src/components/MenuBar.js
@@ -12,42 +12,38 @@ const MenuBar = () => {
   const handleItemClick = (e, { name }) => setActiveItem(name);
 
   const menuBar = user ? (
-    <div>
-      <Menu pointing secondary color="teal">
-        <Menu.Item name={user.username} active as={Link} to="/" />
-        <Menu.Menu position="right">
-          <Menu.Item name="logout" onClick={logout} />
-        </Menu.Menu>
-      </Menu>
-    </div>
+    <Menu pointing secondary color="teal">
+      <Menu.Item name={user.username} active as={Link} to="/" />
+      <Menu.Menu position="right">
+        <Menu.Item name="logout" onClick={logout} />
+      </Menu.Menu>
+    </Menu>
   ) : (
-    <div>
-      <Menu pointing secondary color="teal">
+    <Menu pointing secondary color="teal">
+      <Menu.Item
+        name="home"
+        active={activeItem === "home"}
+        onClick={handleItemClick}
+        as={Link}
+        to="/"
+      />
+      <Menu.Menu position="right">
         <Menu.Item
-          name="home"
-          active={activeItem === "home"}
+          name="login"
+          active={activeItem === "login"}
           onClick={handleItemClick}
           as={Link}
-          to="/"
+          to="/login"
         />
-        <Menu.Menu position="right">
-          <Menu.Item
-            name="login"
-            active={activeItem === "login"}
-            onClick={handleItemClick}
-            as={Link}
-            to="/login"
-          />
-          <Menu.Item
-            name="register"
-            active={activeItem === "register"}
-            onClick={handleItemClick}
-            as={Link}
-            to="/register"
-          />
-        </Menu.Menu>
-      </Menu>
-    </div>
+        <Menu.Item
+          name="register"
+          active={activeItem === "register"}
+          onClick={handleItemClick}
+          as={Link}
+          to="/register"
+        />
+      </Menu.Menu>
+    </Menu>
   );
 
   return menuBar;
