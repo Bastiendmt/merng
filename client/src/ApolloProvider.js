fab22798f973641dd93e74de7f8a7d93c8803659
bastienxs <bastienxs@gmail.com>
2021-04-14 11:49:31 +0200

    api url

--- client/src/ApolloProvider.js
+++ client/src/ApolloProvider.js
@@ -9,7 +9,7 @@ import {
 import { setContext } from "apollo-link-context";
 
 const httpLink = createHttpLink({
-  uri: "http://localhost:4000",
+  uri: "https://fast-anchorage-79743.herokuapp.com/",
 });
 
 const authLink = setContext(() => {
